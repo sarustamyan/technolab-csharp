a211b5fdf49aa4b1d5896f7f040325fb07bdef29
Sam Arustamyan <samvel.arustamyan@gmail.com>
2022-10-28 11:54:39 +0400

    Lesson 4

---
+++ lessons/Lesson4/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] items = new int[] { -1, -2, -45, -16, -77, -8 };
+int max = int.MinValue;
+for (int i = 0; i < items.Length; i++)
+{
+    if (items[i] > max)
+    { 
+        max = items[i];
+    }
+}
+Console.WriteLine(max);
